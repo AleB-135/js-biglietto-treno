33f4724fc41fbf36d2d6a46e1d526a32216f37f4
AleB-135 <alessiobrusca1999@outlook.com>
2025-02-18 17:22:55 +0100

    feat: Aggiunto prezzo finale comprensivo di sconto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
-const kilometers = prompt("Quanti chilometri vuoi percorrere?")
-const age = prompt("Quanti anni hai?")
-console.log(kilometers, age);
+// RACCOLTA DATI
 
+const kilometersStr = prompt("Quanti chilometri vuoi percorrere?")
+const ageStr = prompt("Quanti anni hai?")
+console.log(kilometersStr, ageStr);
+
+// BONUS
+const kilometers = parseInt(kilometersStr);
+const age = parseInt(ageStr);
+console.log(kilometers,age)
+// FINE BONUS
 
 
 const ticketPrice = (0.21 * kilometers);
 console.log(ticketPrice);
- 
 
-let message;
+
+// ESECUZIONE LOGICA
+
+let discount;
 if(age < 18) {
-    console.log(ticketPrice * 20 / 100);    
-} else if (age > 65) {
-    console.log(ticketPrice * 40 / 100);
+     console.log(ticketPrice * 20 / 100);    
+} else if (age > 65) { 
+     console.log(ticketPrice * 40 / 100);
+} else {
+     console.log(ticketPrice)
+}
+
+// OUTPUT
+let finalPrice;
+if(age < 18) {
+     console.log(ticketPrice - ticketPrice * 20 / 100);
+} else if (age > 65) { 
+     console.log(ticketPrice - ticketPrice * 40 / 100);
+} else {
+     console.log(ticketPrice);
 }
